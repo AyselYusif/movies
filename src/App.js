1a2f057f0113d8523a6521536d2e4e09f3b7aaaf
Aysel Yusif <ayselyusibova314@gmail.com>
2022-05-27 17:51:26 +0400

    last

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Router } from "react-router-dom";
+import { Route } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 import ListPage from "./pages/ListPage/ListPage";
 
