bb826878017449bfb21bac7d736b474b040b1619
Fikaroo <fuadbayramv@gmail.com>
2022-05-29 12:08:09 +0400

    changes : movie button interactive

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addMovieList } from "../../redux/actions/actions";
 import "./MovieItem.css";
@@ -6,6 +8,16 @@ import "./MovieItem.css";
 function MovieItem({ Title, Year, Poster, imdbID, disabled }) {
   const dispatch = useDispatch();
   const linkActive = useSelector((state) => state.linkActive);
+  const listMovies = useSelector((state) => state.listMovies);
+  const [isAdded, SetAdded] = useState(false);
+  const handlerBtn = (id) => {
+    SetAdded(listMovies.find((item) => item.imdbID === id));
+  };
+
+  useEffect(() => {
+    handlerBtn(imdbID);
+  });
+
   return (
     <article className="movie-item">
       <img className="movie-item__poster" src={Poster} alt={Title} />
@@ -16,10 +28,13 @@ function MovieItem({ Title, Year, Poster, imdbID, disabled }) {
         <button
           type="button"
           className="movie-item__add-button"
-          onClick={() => dispatch(addMovieList(imdbID))}
+          onClick={() => {
+            handlerBtn(imdbID);
+            dispatch(addMovieList(imdbID));
+          }}
           disabled={disabled || linkActive}
         >
-          Добавить в список
+          {!isAdded ? "Добавить в список" : "Добавлено"}
         </button>
       </div>
     </article>
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -18,18 +18,22 @@ const reducer = (state = initialState, { type, payload }) => {
     case ADD_MOVIE_TO_LIST:
       const movie = state.movies.find((item) => item.imdbID === payload);
       return { ...state, listMovies: [...state.listMovies, movie] };
+
     case REMOVE_MOVIE_FROM_LIST:
       const newListMovies = state.listMovies.filter(
         (item) => item.imdbID !== payload
       );
       return { ...state, listMovies: newListMovies };
+
     case ADD_MOVIES:
       return { ...state, movies: payload };
+
     case SET_LINKACTIVE:
       return { ...state, linkActive: payload };
 
     case ADD_POST_MOVIES:
       return { ...state, postMovies: [...state.postMovies, { ...payload }] };
+
     default:
       return state;
   }
